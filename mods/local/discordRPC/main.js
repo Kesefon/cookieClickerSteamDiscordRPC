6e70b53165b5d0a8a28b7b9550690e47ec6c79c5
Kesefon <kesefon+github@protonmail.com>
2021-10-13 00:14:46 +0200

    frenzy indicator

--- mods/local/discordRPC/main.js
+++ mods/local/discordRPC/main.js
@@ -1,11 +1,18 @@
 var lastCookiesPerSecond = 0;
 var date = Date.now();
+var smallImage;
 
 async function setActivity() {
+	if(Game.hasBuff('Frenzy')) {
+		smallImage = 'frenzy';
+	} else {
+		smallImage = undefined;
+	}
   window.api.send('discord', {
     details: Beautify(lastCookiesPerSecond)+' Cookies per Second',
     state: Beautify(Game.cookiesEarned) + ' Cookies earned',
     startTimestamp: date,
+	smallImageKey: smallImage,
     largeImageKey: 'icon',
     largeImageText: 'Using Kesefon\'s rich presence',
   });
